441fd9d5442e0b631a555fa19f644a73722a5627
Jake <JakeSESaunders@gmail.com>
2024-03-15 15:44:03 +0000

    Made goofy title screen.

---
+++ Network.gd
@@ -0,0 +1,16 @@
+extends Node
+
+const DEFAULT_IP = "127.0.0.1"
+const DEFAULT_PORT = 31400
+const MAX_PLAYERS = 5
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
